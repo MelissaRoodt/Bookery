83b2a08e7cdf0581e2cd17f904627889ccff4acc
Melissa Roodt <melissaroodt336@gmail.com>
2024-06-21 14:09:07 +0200

    Add a method to add a new book

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ import moment from "moment";
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "Bookery",
+    password: "123456",
+    port: 5432,
+});
+
+db.connect();
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -49,6 +59,27 @@ app.get("/", async (req, res) => {
 
 app.get("/add", (req, res) => {
     res.render("add.ejs");
+});
+
+app.post("/add", async (req, res) =>{
+    const book = {
+        title: req.body["title"],
+        isbn: req.body["isbn"],
+        rating: req.body["rating"],
+        author: req.body["author"], 
+        date: req.body["date"], 
+        summary: req.body["summary"]
+    }
+
+    try{
+        const results = await db.query("INSERT INTO books (title, author, isbn, rating, date, summary) VALUES ($1, $2, $3, $4, $5, $6)",
+            [book.title, book.author, book.isbn, book.rating, book.date, book.summary]);
+        
+            res.redirect("/");
+    }catch (err) {
+      console.log(err);
+      res.redirect("/");
+    }
 });
 
 app.get("/update", (req, res) => {
